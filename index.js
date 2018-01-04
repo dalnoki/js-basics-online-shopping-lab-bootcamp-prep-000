58dbf923f140f8e177726301a56487af82dd0d5d
Zsófia Dalnoki-Papp <dalnokipapp.zsofia@gmail.com>
2018-01-04 11:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addToCart(item) {
   var itemToCart = {};
   itemToCart[key] = Math.floor(Math.random() * 100) + 1;
   cart.push(obj);
-  console.log(´${key} has been added to your cart.´);
+  console.log(`${key} has been added to your cart.`);
   return cart;
 
 
